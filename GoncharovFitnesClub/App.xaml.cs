d9b5962b2dc33cddcc67afbd681c8a5daace0f62
AlexGnhrov <gonharov.alex@ya.ru>
2023-03-27 19:20:40 +0300

    A

--- GoncharovFitnesClub/App.xaml.cs
+++ GoncharovFitnesClub/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using GoncharovFitnesClub.WindoFolder;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,8 +14,25 @@ namespace GoncharovFitnesClub
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
+    /// 
+  
     public partial class App : Application
     {
+        Mutex mutex;
+        App()
+        {
+            bool createdNew;
+
+            mutex = new Mutex(true, "GoncharovFitnesClub", out createdNew);
+
+            if (!createdNew)
+            {
+                App.Current.Shutdown();
+                App.Current.MainWindow.Focus();
+            }
+        }
+
 
     }
 }
+
